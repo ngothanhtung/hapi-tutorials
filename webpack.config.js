695ad958fcda9c5755ca6e6e76223f6cb0ae75dd
Ngo Thanh Tung <tungnt@softech.vn>
2017-10-11 08:46:53 +0700

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,27 +5,42 @@ var path = require('path');
 module.exports = {
   context: path.join(__dirname, "client"),
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./src/js/index.js",
+  entry: {
+    react: ["react", "react-dom"],
+    index: "./src/js/index.js",
+  },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+        plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
       }
-    ]
+    }]
   },
   output: {
     path: __dirname + "/public/js/",
-    filename: "index.min.js"
+    filename: "[name].min.js"
   },
-  plugins: debug ? [] : [
+  plugins: debug ? [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'react',
+      filename: '[name].min.js',
+      minChunks: Infinity
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'bundle',
+      chunks: ['index', 'about'],
+      filename: '[name].min.js',      
+    })
+  ] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      sourcemap: false
+    })
+  ]
 };
